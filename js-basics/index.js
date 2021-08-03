aadcb5fb7be46d6625c9019aa88a1409a1577f09
Wiikwik <31203426+wiikwik@users.noreply.github.com>
2021-08-03 23:14:37 +0200

    Add files via upload

---
+++ js-basics/index.js
@@ -0,0 +1,52 @@
+        //comment my first java Script code
+        console.log('Hello Vik JS in separate file');
+
+     let name = 'Vik'; //string literal
+      console.log(name);
+
+        //cannot be reserved keyword
+        //should be meaningful
+        //cannot start with a number
+        //cannot contain space or hyphen(-)
+        //are cse-sensitive
+        //use camelCase
+
+      let firstName ='Victoria';
+      let lastName ='Gontarik';
+      console.log(firstName,lastName);
+
+      const interestRate = 0.3;
+      console.log(interestRate);
+
+      let age=38; //number literal
+
+      let isApproved=true; //boolean literal
+      let isDeclined =false;
+
+      let selectedColor=null; //when we want to clear the vlue of the var
+      let selectedType = undefined;
+
+      var addition=4+4;
+      console.log(addition);
+
+      let  addStrings=  "4" + "4";
+console.log(addStrings);
+
+let  subtruct= 1299354454-128;
+console.log(subtruct);
+
+let  multipl=12*100
+console.log(multipl);
+
+let  div= 500/10
+console.log(div);
+
+let x = 5;
+let y = 2;
+let modulus = x % y;
+console.log(modulus);
+
+
+
+
+
