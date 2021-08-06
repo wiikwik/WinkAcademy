5601ea71b4f9165e846238fc57ceffd2800d7de8
Wiikwik <31203426+wiikwik@users.noreply.github.com>
2021-08-06 13:11:42 +0200

    Add files via upload

---
+++ opdrachtJSIf/index.js
@@ -0,0 +1,30 @@
+// pub opdracht
+
+const age=10;
+console.log(age);
+
+if (age>=18){
+  document.write("allow to enter");
+}else {
+    document.write("not allow to enter");
+  }
+  console.log();
+
+
+const isFemale=true;
+
+if (false){
+  document.write("free entrance");
+}else {
+    document.write("buy a ticket");
+  }
+  console.log();
+
+  const driverStatus = 'bob';
+
+  if (driverStatus){
+    document.write("is allowed to drive");
+  }else {
+      document.write("is not allowed to drive");
+    }
+    console.log();
