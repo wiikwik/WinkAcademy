df785f3226848286a6e623b068bdfb3ac2f636a9
Wiikwik <31203426+wiikwik@users.noreply.github.com>
2021-08-10 19:14:38 +0200

    Add files via upload

---
+++ js-basics-function-opdracht/function.js
@@ -0,0 +1,36 @@
+// makePizza opdracht function
+const makePizza= function() {
+
+  console.log("Make a dough");
+  console.log("Roll out the dough");
+  console.log("Top the pizza");
+  console.log("Bake the pizza");
+
+  document.write("Make a dough");
+  document.write("Roll out the dough");
+  document.write("Top the pizza");
+  document.write("Bake the pizza");
+}
+
+const makeSushi= function() {
+
+  console.log("Prepare the Rice and Nor");
+  console.log("pread the Rice on the Nori");
+  console.log("Add Fillings to the Bottom");
+  console.log("Roll the Sushi");
+
+  document.write("Prepare the Rice and Nor");
+  document.write("pread the Rice on the Nori");
+  document.write("Add Fillings to the Bottom");
+  document.write("Roll the Sushi");
+
+}
+
+
+// using a function
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
