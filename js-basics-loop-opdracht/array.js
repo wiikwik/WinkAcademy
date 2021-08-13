5b63d59a2d7a405f7effbfc3775745fff7921d4b
Wiikwik <31203426+wiikwik@users.noreply.github.com>
2021-08-13 16:07:25 +0200

    Add files via upload

---
+++ js-basics-loop-opdracht/array.js
@@ -0,0 +1,26 @@
+const colors = ["blue", "gold", "green"];
+
+let i = 0;
+//while
+
+while (i< colors.length) {
+    console.log(colors[i]);
+    i++;
+  };
+
+
+
+// for
+for (i=0; i<colors.length; i++) {
+    console.log(colors[i]);
+}
+
+
+// const myColorArray = ["yellow", "blue", "red", "orange"];
+
+// // oude manieren
+// let i = 0;
+// while (i < myColorArray.length) {
+//   console.log(myColorArray[i]);
+//   i++;
+// }
