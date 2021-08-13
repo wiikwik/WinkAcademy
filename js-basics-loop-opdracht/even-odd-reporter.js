33985f7129c3a69521ea30a70f711b398edb1ab7
Wiikwik <31203426+wiikwik@users.noreply.github.com>
2021-08-13 11:39:06 +0200

    Add files via upload

---
+++ js-basics-loop-opdracht/even-odd-reporter.js
@@ -0,0 +1,16 @@
+
+// for (let i = 0; i <= 20; i++) {
+//     console.log("Is " +i + " even or odd?");
+  
+//   };
+  
+ 
+  for(let i =0; i<=20 ;i++) {
+
+   if (i%2==0) {
+    console.log(+i+ " is even");
+   }else
+    console.log( +i+  " is odd")
+  };
+    
+   
---
+++ js-basics-loop-opdracht/multiplication-tables.js
@@ -0,0 +1,17 @@
+
+
+var a=9;
+for  (i=1; i<=9; i++) {
+    console.log(+i+ " multiply 9 is: " +i*a);
+}
+console.log("you are now all calculated")
+
+
+// bonus
+
+for  (i=1; i<=9; i++) {
+    for  (b=1; b<=9; b++) {
+    console.log(+i+ " multiply " +b+ " is: " +i*b);
+};
+};
+console.log("you are now all calculated")
