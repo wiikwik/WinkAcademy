adbf09563df7a2363b6eef8a3c0e5a50f8931c26
Wiikwik <31203426+wiikwik@users.noreply.github.com>
2021-08-18 16:57:49 +0200

    Add files via upload

--- domFinalOpdracht/domFinalOpdracht.js
+++ domFinalOpdracht/domFinalOpdracht.js
@@ -1,9 +1,25 @@
 
         function myFunction() {
-            var x = document.getElementById("myColors");
+            var x = document.getElementById("list");
             if (x.style.display === "block") {
                 x.style.display = "none";
             } else {
                 x.style.display = "block";
             }
-        }
+        };
+
+
+          function Home() {
+            document.querySelector(".backgound").style.backgroundColor = "white";
+          };
+        
+          function Green() {
+            document.querySelector(".backgound").style.backgroundColor = "lightgreen";
+          };
+          function Yellow() {
+            document.querySelector(".backgound").style.backgroundColor = "yellow";
+          };
+          function Blue() {
+            document.querySelector(".backgound").style.backgroundColor = "lightblue";
+          };
+        
